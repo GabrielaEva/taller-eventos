c55f7bd03ce8cbb6bce3f94056dd0ff630502c61
GabrielaEva <ggab431@gmail.com>
2025-08-19 23:46:21 -0300

    Soluciono propagación del click con stopPropagation

--- eventos.js
+++ eventos.js
@@ -2,3 +2,9 @@ let div = document.getElementById("miDiv");
 div.addEventListener("click", function() {
     alert("Hola soy el Div!");
 });
+
+let boton = document.querySelector("#miDiv button");
+boton.addEventListener("click", function(event) {
+    alert("Hola!");
+    event.stopPropagation(); 
+});
